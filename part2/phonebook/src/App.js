8ade6ecc5d4206ca50a72f20cce86b73ef7cc59e
thedambar <dambar@gmail.com>
2022-10-08 09:41:15 +0300

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 import Persons from './components/Persons'
+import Filter from './components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '0401234567' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (event) => setNewName(event.target.value);
 
   const handleNumberChange = (event) => setNewNumber(event.target.value);
-
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -20,13 +24,16 @@ const App = () => {
       alert(`${newName} is already in phonebook`);
       return;
     }
-    setPersons(persons.concat({name: newName, number: newNumber}));
+    const newId = Math.max(persons.map(person => person.id))+1;
+    setPersons(persons.concat({name: newName, number: newNumber, id: newId}));
     setNewName('');
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -39,7 +46,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={persons} filter={filter}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,5 +1,13 @@
 const Person = ({person}) => <div>{person.name} {person.number}</div>
 
-const Persons = ({persons}) => persons.map((person) => <Person key={person.name} person={person} />)
+const Persons = ({persons, filter}) => {
+  const filteredPersons = filter !== '' ? 
+    persons.filter((person) => 
+      person.name.toString().toLowerCase().includes(filter.toString().toLowerCase())) :
+    persons
+  
+  console.log(filteredPersons);
+  return filteredPersons.map((person) => <Person key={person.id} person={person} />)
+}
 
 export default Persons;
